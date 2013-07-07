01d98e248f26bc8b0ba8a5b39951e91bf34c53a5
Mehul Mandania <mehul.mandania@gmail.com>
2013-07-07 11:56:08 +0000

    no.7 try2

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var fs = require('fs');
     var buf = new buffer(fs.readFileSync("index.html", "utf-8"));    
-    response.send(buffer.toString());
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
