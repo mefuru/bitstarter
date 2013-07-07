82ae672658b237f9f633d45f76d2282d35bc7d8a
Mehul Mandania <mehul.mandania@gmail.com>
2013-07-07 12:56:14 +0000

    rollback

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-    var fs = require('fs');
-    response.send(fs.readFileSync("index.html", "utf-8");
-});
+ app.get('/', function(request, response) {
+     var fs = require('fs');
+     var buf = new buffer(fs.readFileSync("index.html", "utf-8"));    
+     response.send(buffer.toString());
+     response.send(buf.toString());
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
